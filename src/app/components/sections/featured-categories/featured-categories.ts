d8acd1b015d64b9f18a992283d246558a6b12af6
bunsen <bunsen@bunsen.bunsen>
2025-08-25 21:10:13 -0500

    buscador

--- src/app/components/sections/featured-categories/featured-categories.ts
+++ src/app/components/sections/featured-categories/featured-categories.ts
@@ -162,7 +162,7 @@ export class FeaturedCategories implements AfterViewInit, OnDestroy {
             0: { items: 1 },
             576: { items: 2 },
             992: { items: 3 },
-            1200: { items: 4 }
+            1200: { items: 5 }
           }
         });
         this.initialized = true;
